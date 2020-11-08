bc214ee662e98824d9ce1ca8b5077bbda84317d5
Atishay Srivastava <42544589+xAtishayx@users.noreply.github.com>
2020-11-08 06:44:06 +0530

    fixed UI

---
+++ Assets/scripts/CreatorsSceneManager.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Xml.Serialization;
+using UnityEngine.UI;
+
+public class CreatorsSceneManager : MonoBehaviour
+{
+    public GameObject placeAddedUnitPrefab;
+    public GameObject pathsAddedUnitPrefab;
+    public Text consoleAddedNew;
+    public Text consoleAddedLast;
+    public string LastAdded = "";
+    public float unitHeight = 130;
+    public RectTransform pathsAdded;
+    public RectTransform placesAdded;
+    public int noOfPlacesAdded = 0;
+    public int noOfPathsAdded = 0;
+
+    //deletes elements in paths added and paths places and resets everything in case the mapping goes wrong
+    public void Reset()
+    {
+        foreach (Transform t in pathsAdded.GetComponentsInChildren<Transform>())
+            Destroy(t.gameObject);
+
+        foreach (Transform t in placesAdded.GetComponentsInChildren<Transform>())
+            Destroy(t.gameObject);
+
+        placesAdded.sizeDelta = new Vector2(placesAdded.sizeDelta.x, 0);
+        pathsAdded.sizeDelta = new Vector2(pathsAdded.sizeDelta.x, 0);
+
+        noOfPathsAdded = 0;
+        noOfPlacesAdded = 0;
+    }
+
+    public void AddPlace(string placeName)
+    {
+        noOfPlacesAdded++;
+        GameObject temp = Instantiate(placeAddedUnitPrefab, placesAdded);
+        temp.name = placeName;
+        temp.GetComponentInChildren<Text>().text = placeName;
+        placesAdded.sizeDelta = new Vector2(placesAdded.sizeDelta.x, noOfPlacesAdded * unitHeight);
+    }
+
+    public void AddPath(string pathName)
+    {
+        noOfPathsAdded++;
+        GameObject temp = Instantiate(pathsAddedUnitPrefab, pathsAdded);
+        temp.name = pathName;
+        temp.GetComponentInChildren<Text>().text = pathName;
+        pathsAdded.sizeDelta = new Vector2(pathsAdded.sizeDelta.x, noOfPathsAdded * unitHeight);
+    }
+
+    public void StartMapping()
+    {
+        //start the mapping
+        Debug.Log("start mapping called");
+    }
+
+    public void EndMapping()
+    {
+        SaveData();
+        DataManagerCreator.ins.UploadeFile();
+        //end the mapping and upload the file
+        Debug.Log("end mapping called");
+    }
+
+    public void WriteToConsole(string txt)
+    {
+        consoleAddedNew.text = txt;
+        consoleAddedLast.text = LastAdded;
+        LastAdded = txt;
+    }
+
+    public void SaveData()
+    {
+        mapData md = new mapData();
+        GameObject[] gbPaths = GameObject.FindGameObjectsWithTag("path");
+        GameObject[] gbPlaces = GameObject.FindGameObjectsWithTag("place");
+        GameObject[] gbtags = GameObject.FindGameObjectsWithTag("product");
+
+        md.noOfPaths = gbPaths.Length;
+        md.noOfPlaces = gbPlaces.Length;
+        md.noOftags = gbtags.Length;
+        foreach (GameObject gb in gbPaths)
+        {
+            paths p = new paths();
+
+            p.name = gb.name;
+            p.position = gb.transform.position;
+            p.noOfPositions = gb.GetComponent<LineRenderer>().positionCount;
+            p.lineRendPositions = new Vector3[p.noOfPositions];
+
+            gb.GetComponent<LineRenderer>().GetPositions(p.lineRendPositions);
+
+            md.pathsList.Add(p);
+        }
+
+        foreach (GameObject gb in gbPlaces)
+        {
+            places p = new places();
+
+            p.name = gb.name;
+            p.position = gb.transform.position;
+
+            md.placesList.Add(p);
+        }
+        foreach (GameObject gb in gbtags)
+        {
+            tags p = new tags();
+
+            p.name = gb.name;
+            p.position = gb.transform.position;
+
+            md.tagsList.Add(p);
+        }
+
+        //xml saving script
+
+        XmlSerializer serializer = new XmlSerializer(typeof(mapData));
+        FileStream stream = new FileStream(Application.persistentDataPath + "/mapdata.xml", FileMode.Create);
+        serializer.Serialize(stream, md);
+        Debug.Log("File created");
+        stream.Close();
+
+        DataManagerCreator.ins.UploadeFile();
+
+    }
+    public void closeapp()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/scripts/DataManagerCreator.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Xml.Serialization;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using Firebase.Storage;
+using System;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+using TMPro;
+
+
+public class DataManagerCreator : MonoBehaviour
+{
+    public static DataManagerCreator ins;
+
+    [SerializeField]
+    private string ID;
+
+    public void Awake()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                //   app = Firebase.FirebaseApp.DefaultInstance;
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+
+        if(ins == null)
+        {
+            ins = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void LoginToCreator()
+    {
+        ID = GameObject.FindGameObjectWithTag("ID").GetComponent<TextMeshProUGUI>().text;
+    }
+
+    public string CreateHash(string id)
+    {
+        MD5 md5Hash = MD5.Create();
+        byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(id));
+        StringBuilder sBuilder = new StringBuilder();
+        for (int i = 0; i < data.Length; i++)
+        {
+            sBuilder.Append(data[i].ToString("x2"));
+        }
+
+        Debug.Log(id + " " + sBuilder.ToString());
+
+        return sBuilder.ToString();
+    }
+
+    public void UploadeFile()
+    {
+        Debug.Log("uploading file");
+
+        // Get a reference to the storage service, using the default Firebase App
+        FirebaseStorage storage = FirebaseStorage.DefaultInstance;
+
+        StorageReference storage_ref = storage.GetReferenceFromUrl("gs://pathiktestsih.appspot.com");
+
+        // File located on disk
+        string local_file = "file://" + Application.persistentDataPath + "/mapdata.xml";
+
+        Debug.Log(local_file);
+
+        // Create a reference to the file you want to upload
+        // StorageReference file_ref = storage_ref.Child("Map Data/" + CreateHash(Random.Range(100f, 1000f).ToString()) + "/mapdata.xml");
+
+        // Create file metadata including the content type
+        var new_metadata = new MetadataChange();
+        new_metadata.ContentType = "text/xml";
+
+
+        var task = storage_ref.Child("Map Data/" + CreateHash(ID) + "/mapdata.xml")
+    .PutFileAsync(local_file, null,
+      new StorageProgress<UploadState>(state => {
+          // called periodically during the upload
+          Debug.Log(String.Format("Progress: {0} of {1} bytes transferred.",
+                             state.BytesTransferred, state.TotalByteCount));
+      }), CancellationToken.None, null);
+
+        task.ContinueWith(resultTask => {
+            if (!resultTask.IsFaulted && !resultTask.IsCanceled)
+            {
+                Debug.Log("Upload finished.");
+                ID = "null";
+            }
+            else
+            {
+                Debug.Log(task.Exception.ToString());
+            }
+        });
+    }
+
+}
+
+
+public class mapData
+{
+    public int noOfPaths;
+    public int noOfPlaces;
+    public int noOftags;
+    public List<paths> pathsList = new List<paths>();
+    public List<places> placesList = new List<places>();
+    public List<tags> tagsList = new List<tags>();
+}
+
+public class paths
+{
+    public string name;
+    public Vector3 position;
+
+    //Line renderer positions
+
+    public int noOfPositions;
+    public Vector3[] lineRendPositions;
+}
+
+public class places
+{
+    public string name;
+    public Vector3 position;
+}
+
+public class tags
+{
+    public string name;
+    public Vector3 position;
+}
